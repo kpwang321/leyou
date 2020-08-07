fb0bccaf7cc4fee44226ccd869aa8e6d4016e957
kpwang <kpwang321@163.com>
2020-08-07 18:45:21 +0800

    rabbitmq

---
+++ leyou/leyou-goods-web/src/main/java/com/leyou/goods/client/SpecificationClient.java
@@ -0,0 +1,12 @@
+package com.leyou.goods.client;
+
+import com.leyou.item.api.SpecificationApi;
+import org.springframework.cloud.openfeign.FeignClient;
+
+/**
+ * @author kpwang
+ * @create 2020-08-06 0:26
+ */
+@FeignClient("item-service")
+public interface SpecificationClient extends SpecificationApi {
+}
